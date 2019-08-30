5e2cd485a5857062f51049eccb2688e4d14963eb
Victor-733 <victorwahid@gmail.com>
2019-08-30 12:35:12 +0000

    start working on bounce animation

--- animation.js
+++ animation.js
@@ -3,10 +3,19 @@ const tl = new TimelineMax({repeat: -1});
 tl.to("#smoke1", 2, {opacity: 0, y: -90});
 tl.to("#smoke2", 2, {opacity: 0, y: -90}, "-=1");
 
-const logo = document.getElementById("logo");
-const hoverOver = logo.addEventListener("mouseover", splat);
+const logo = document.getElementById("background");
+logo.addEventListener("mouseover", splat);
 
+let timeline = new TimelineMax({});
+isplaying = false;
 function splat() {
-    TweenMax.to("#logo", 0.5, {y: -80});
+    if(isplaying == false){
+        isplaying = true;
+        console.log(isplaying);
+        timeline.to(".logo", 0.2, {y: -30});
+        timeline.to(".logo", 0.5, {y: 0, ease:Bounce.easeOut});
+        isplaying = false;
+        console.log(isplaying);
+    }
 }
 
