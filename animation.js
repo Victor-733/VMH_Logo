76eac04b4eef077977d749a42da1be7c4e83f207
Victor-733 <victorwahid@gmail.com>
2019-08-30 11:31:41 +0000

    smoke animation

--- animation.js
+++ animation.js
@@ -1,3 +1,12 @@
-const tl = new TimeLineMax({repeat: -1});
-    tl.to("#smoke1", 2, {opacity: 0, y: -90});
-    tl.to("#smoke2", 2, {opacity: 0, y: -90});
+const tl = new TimelineMax({repeat: -1});
+
+tl.to("#smoke1", 2, {opacity: 0, y: -90});
+tl.to("#smoke2", 2, {opacity: 0, y: -90}, "-=1");
+
+const logo = document.getElementById("logo");
+const hoverOver = logo.addEventListener("mouseover", splat);
+
+function splat() {
+    TweenMax.to("#logo", 0.5, {y: -80});
+}
+
