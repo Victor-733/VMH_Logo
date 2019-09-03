0c73e8c06f4c1b03582baca882234998bd274d9a
Victor-733 <victorwahid@gmail.com>
2019-09-03 14:42:15 +0000

    finish bounce animation

--- animation.js
+++ animation.js
@@ -3,19 +3,24 @@ const tl = new TimelineMax({repeat: -1});
 tl.to("#smoke1", 2, {opacity: 0, y: -90});
 tl.to("#smoke2", 2, {opacity: 0, y: -90}, "-=1");
 
-const logo = document.getElementById("background");
-logo.addEventListener("mouseover", splat);
+const logo = document.getElementById("cup-handle");
+logo.addEventListener("mouseenter", splat);
 
-let timeline = new TimelineMax({});
-isplaying = false;
-function splat() {
-    if(isplaying == false){
-        isplaying = true;
-        console.log(isplaying);
-        timeline.to(".logo", 0.2, {y: -30});
-        timeline.to(".logo", 0.5, {y: 0, ease:Bounce.easeOut});
+let timeline = new TimelineMax({
+    onComplete(){
         isplaying = false;
-        console.log(isplaying);
+        timeline.pause(0);
+    }
+});
+
+isplaying = false;
+
+timeline.to(".logo", 0.2, {y: -30});
+timeline.to(".logo", 0.5, {y: 0, ease:Bounce.easeOut});
+timeline.pause();
+
+function splat() {
+    if(!isplaying){
+        timeline.play();
     }
 }
-
